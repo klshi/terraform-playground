eed2e9cdb1cca69327d775dc32fef4742100a003
Kailun Shi <kailunshi@microsoft.com>
2021-09-30 13:56:06 +0800

    create vn

--- main.tf
+++ main.tf
@@ -23,3 +23,9 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+resource "azurerm_virtual_network" "vn" {
+  name                = "kailunvn"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
